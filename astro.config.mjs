05fa4a7dfc7095d93d92382a164b32e2627fb13a
Diego <diegoandresrm43210@gmail.com>
2026-04-14 11:26:04 -0500

    creacionverceljson

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    functionPerRoute: true,
+    edgeMiddleware: false
+  }),
 
   vite: {
     plugins: [tailwindcss()]
